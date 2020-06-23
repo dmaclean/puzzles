60372b31ba03170bda3befe5c06e89f20c41de47
dmaclean <dan.maclean@victiv.com>
2020-06-23 11:15:06 -0400

    Refactor

--- hackerrank/easy/lisas_workbook/l.py
+++ hackerrank/easy/lisas_workbook/l.py
@@ -8,17 +8,21 @@ def workbook(n, k, arr):
     special = 0
     curr_page = 1
     for idx in range(1, n + 1):
-        num_problems = arr[idx - 1]
-        print(f'Number of problems for chapter {idx} is {num_problems}')
-        pages = (num_problems // k) + (1 if num_problems % k > 0 else 0)
+        special += process_chapter(arr, curr_page, idx, k)
+    return special
 
-        for p in range(1, num_problems + 1):
-            page_num = curr_page + ((p - 1) // k)
-            if page_num == p:
-                print(f'Problem {p} is on page {page_num}')
-                special += 1
 
-        curr_page += pages
+def process_chapter(arr, curr_page, idx, k):
+    special = 0
+    num_problems = arr[idx - 1]
+    print(f'Number of problems for chapter {idx} is {num_problems}')
+    pages = (num_problems // k) + (1 if num_problems % k > 0 else 0)
+    for p in range(1, num_problems + 1):
+        page_num = curr_page + ((p - 1) // k)
+        if page_num == p:
+            print(f'Problem {p} is on page {page_num}')
+            special += 1
+    curr_page += pages
     return special
 
 
